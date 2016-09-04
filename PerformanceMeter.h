0d99609061ca85b26aa67bc5f11efa176d8ddb8a
HL65536 <HL65536@web.de>
2016-09-05 00:37:00 +0200

    Update PerformanceMeter.h

--- PerformanceMeter.h
+++ PerformanceMeter.h
@@ -3,6 +3,11 @@
 #include "SFML\Window.hpp"
 #include <string>
 
+#define FLAG_NOW 1
+#define FLAG_AVG 2
+#define FLAG_SPIKES 4
+#define FLAG_ALL_TIME_MAX 8
+
 class PerformanceMeter
 {
 public:
@@ -15,19 +20,34 @@ public:
 	int getMaxMeasured(int stepID);
 	int getTime(int stepID);
 	float getAVG(int stepID);
+	float getSpikes(int stepID);
 	void setMaxTolerated(int stepID, int us);
+	void PerformanceMeter::setName(std::string name, int  stepID);
+	std::string getInfo(int stepID,int infoFlags);
+	void reset();
+	void clear();
+	void setSpikeHalfLifeTime(float seconds);
 
 private:
+	
+	void exceededMax(int stepID);
 
-	sf::Clock clock;
+	sf::Clock clock; //TODO replace
 	int n;
-	//std::string * stuff; //TODO implement
+
+	std::string * names; //TODO implement
+
 	int * time;
 	int * maxTolerated;
 	int * maxMeasured;
 	float * avg;
-	float avgWeight = 0.01f;
+	float * spikes;
 
-	void exceededMax(int stepID);
+
+	float spikeHalfLifeInv = 1.0f/10000000;
+	float spikeMultiplier=0.00000005f;
+public:
+	float avgWeight = 0.01f;
+	bool useFastApproximation = true;
 };
 
