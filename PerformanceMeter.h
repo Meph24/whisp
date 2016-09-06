65b73f02b54dc1a71669a6d95c9ab19804792afc
HL65536 <HL65536@web.de>
2016-09-06 03:22:03 +0200

    added comments

--- PerformanceMeter.h
+++ PerformanceMeter.h
@@ -11,22 +11,71 @@
 class PerformanceMeter
 {
 public:
-	PerformanceMeter(int count);
+
+	//collects data about the performance of a loop with [stepCount] internal steps
+	PerformanceMeter(int stepCount);
 	~PerformanceMeter();
 
-	//return: exceeded maximum time
+	/*
+	usage:
+	PerformanceMeter pm(3);
+	//[
+		//optional:
+		doStuffThatNeedsTime();
+		pm.reset();
+	//]
+	while(condition)
+	{
+		doThingA();
+		pm.registerTime(0);
+		if(something)
+		{
+			doThingB();
+			pm.registerTime(1);
+		}
+		doThingC();
+		pm.registerTime(2);
+	}
+
+	cout<<"most time ever spent doing thing A: "<<(pm.getMaxMeasured(0)/1000.0f)<<"ms"<<endl;
+	
+	*/
+
+
+
+	//return: if measurement exceeded maximum time
 	bool registerTime(int stepID);
 
+	//clear data about the maximum ever measured time for each step
 	void clearMax();
-	int getMaxMeasured(int stepID);
+
+	//returns the last measured time for the given step (in us)
 	int getTime(int stepID);
+
+	//only works after having collected a lot of data, (time in us)
 	float getAVG(int stepID);
+
+	//=max measured but with a half life time
 	float getSpikes(int stepID);
+
+	//returns maximum time ever measured for the given step (in us)
+	int getMaxMeasured(int stepID);
+
 	void setMaxTolerated(int stepID, int us);
+
+	//give a name to a step so this class can format a string for you
 	void PerformanceMeter::setName(std::string name, int  stepID);
+
+	//returns a string describing the performance of the given step, ready to print
 	std::string getInfo(int stepID,int infoFlags);
+
+	//reset the timer to 0
 	void reset();
+
+	//clears all collected info
 	void clear();
+
+
 	void setSpikeHalfLifeTime(float seconds);
 
 private:
@@ -49,7 +98,11 @@ private:
 	float spikeHalfLifeInv = 1.0f/10000000;
 	float spikeMultiplier=0.00000005f;
 public:
+
+	//avg[stepID] = avg[stepID] * (1 - avgWeight) + t * avgWeight;
 	float avgWeight = 0.01f;
+
+	//use a faster algorithm for applying the half life of spikes (inaccurate)
 	bool useFastApproximation = true;
 };
 
