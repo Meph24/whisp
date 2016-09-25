a66538835b3f2c448305d33abb852cee78a605c4
Meph24 <robert.graf24@gmx.net>
2016-09-25 17:18:07 +0200

    Add files via upload

---
+++ lookup.cpp
@@ -0,0 +1,85 @@
+#ifndef LOOKUP_CPP
+#define LOOKUP_CPP
+
+
+#include "lookup.h"
+
+template <typename T>
+lookup<T>::lookup(unsigned int size):
+size(size),
+validIndex(size, false)
+{
+	arr = new T[size];
+}
+
+template <typename T>
+lookup<T>::~lookup()
+{
+	delete[] arr;
+}
+
+template <typename T>
+lookup_index lookup<T>::nextfreeindex()
+{
+	for (unsigned int i = 0; i < size; i++)
+	{
+		if (!validIndex.get(i)) return i;
+	}
+	return LOOKUP_NOVALIDINDEX;
+}
+
+template <typename T>
+lookup_index lookup<T>::insert(T value)
+{
+	//save insert , do not override existing data
+	lookup_index ret;
+	if (findkey(value, ret)) return LOOKUP_NOVALIDINDEX;
+
+	ret = nextfreeindex();
+	if (ret != LOOKUP_NOVALIDINDEX)
+	{
+		map.insert(std::pair<T, lookup_index>(value, ret));
+		arr[ret] = value;
+		validIndex.set(ret, true);
+	}
+	return ret;
+}
+
+template <typename T>
+void lookup<T>::erase(lookup_index index)
+{
+	if (validIndex.get(index))
+	{
+		validIndex.set(index, false);
+		map.erase(map.find(arr[index]));
+	}
+}
+
+template <typename T>
+bool lookup<T>::findkey(T value, lookup_index& ret)
+{
+	std::map<T, lookup_index>::iterator it = map.find(value);
+	if (it == map.end()) return false;
+	ret =  it->second;
+	return true;
+}
+
+template <typename T>
+bool lookup<T>::findvalue(lookup_index index, T& ret)
+{
+	if (validIndex.get(index)) 
+	{
+		ret = arr[index];
+		return true;
+	}
+	return false;
+}
+
+
+/*
+template <typename T>
+lookup<T>::
+
+*/
+
+#endif
