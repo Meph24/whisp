8de068d5d617a218f11c6308961ecef36fa6dcb6
HL65536 <HL65536@web.de>
2016-10-06 01:06:08 +0200

    ned alles löschen...

--- PerformanceMeter.h
+++ PerformanceMeter.h
@@ -3,31 +3,114 @@
 #include "SFML\Window.hpp"
 #include <string>
 
+#define FLAG_NOW 1
+#define FLAG_AVG 2
+#define FLAG_SPIKES 4
+#define FLAG_ALL_TIME_MAX 8
+#define FLAG_ALL_TIME_MIN 16
+
 class PerformanceMeter
 {
 public:
-	PerformanceMeter(int count);
+
+	//collects data about the performance of a loop with [stepCount] internal steps
+	PerformanceMeter(int stepCount);
 	~PerformanceMeter();
 
-	//return: exceeded maximum time
+	/*
+	usage:
+	PerformanceMeter pm(3);
+	//[
+		//optional:
+		doStuffThatNeedsTime();
+		pm.reset();
+	//]
+	while(condition)
+	{
+		doThingA();
+		pm.registerTime(0);
+		if(something)
+		{
+			doThingB();
+			pm.registerTime(1);
+		}
+		doThingC();
+		pm.registerTime(2);
+	}
+	cout<<"most time ever spent doing thing A: "<<(pm.getMaxMeasured(0)/1000.0f)<<"ms"<<endl;
+	
+	*/
+
+
+
+	//return: if measurement exceeded maximum time
 	bool registerTime(int stepID);
+
+	//clear data about the maximum ever measured time for each step
 	void clearMax();
-	int getMaxMeasured(int stepID);
+
+	//clear data about the minimum ever measured time for each step
+	void clearMin();
+
+	//returns the last measured time for the given step (in us)
 	int getTime(int stepID);
+
+	//only works after having collected a lot of data, (time in us)
 	float getAVG(int stepID);
+
+	//=max measured but with a half life time
+	float getSpikes(int stepID);
+
+	//returns maximum time ever measured for the given step (in us)
+	int getMaxMeasured(int stepID);
+
+	//returns minimum time ever measured for the given step (in us)
+	int getMinMeasured(int stepID);
+
 	void setMaxTolerated(int stepID, int us);
 
-private:
+	//give a name to a step so this class can format a string for you
+	void PerformanceMeter::setName(std::string name, int  stepID);
 
-	sf::Clock clock;
+	//returns a string describing the performance of the given step, ready to print
+	std::string getInfo(int stepID,int infoFlags);
+
+	//reset the timer to 0
+	void reset();
+
+	//clears all collected info
+	void clear();
+
+
+
+
+	void setSpikeHalfLifeTime(float seconds);
+
+private:
+	
+	void exceededMax(int stepID);
+
+	sf::Clock clock; //TODO replace
+
 	int n;
-	//std::string * stuff; //TODO implement
+
+	std::string * names;
+
 	int * time;
 	int * maxTolerated;
 	int * maxMeasured;
+	int * minMeasured;
 	float * avg;
+	float * spikes;
+
+
+	float spikeHalfLifeInv = 1.0f/10000000;
+	float spikeMultiplier=0.00000005f;
+public:
+
+	//avg[stepID] = avg[stepID] * (1 - avgWeight) + t * avgWeight;
 	float avgWeight = 0.01f;
 
-	void exceededMax(int stepID);
+	//use a faster algorithm for applying the half life of spikes (inaccurate)
+	bool useFastApproximation = true;
 };
-
