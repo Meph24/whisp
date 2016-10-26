13a7b732e5aad63d9e1c58f8f1688aab3259da2e
HL65536 <HL65536@web.de>
2016-10-27 01:06:07 +0200

    added subsections

--- Graphics2D.cpp
+++ Graphics2D.cpp
@@ -1,7 +1,6 @@
 #include "Graphics2D.h"
 #include <cmath>
 
-#define TAU 6.283185307179586
 
 Graphics2D::Graphics2D(int circleSegments)
 {
@@ -18,8 +17,19 @@ Graphics2D::Graphics2D(int circleSegments)
 			yCircle[i] = cos(i*mult);
 		}
 	}
-	maxX = 1;
-	maxY = 1;
+	maxi.x = 1;//TODO correct
+	maxi.y = 1;
+	maxStack = 256;//TODO add optional parameter
+	curMax = new point[maxStack];
+	curMax[0] = maxi;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (j == i) emptyMat[i * 4 + j] = 1;
+			else emptyMat[i * 4 + j] = 0;
+		}
+	}
 }
 
 
@@ -30,6 +40,7 @@ Graphics2D::~Graphics2D()
 		delete[] xCircle;
 		delete[] yCircle;
 	}
+	delete[] curMax;
 }
 
 void Graphics2D::beginDraw()
@@ -44,14 +55,101 @@ void Graphics2D::endDraw()
 
 float Graphics2D::getMaxX()
 {
-	return maxX;
+	return maxi.x;
 }
 
 float Graphics2D::getMaxY()
 {
-	return maxY;
+	return maxi.y;
+}
+float Graphics2D::getCurMaxX()
+{
+	return curMax[stack].x;
 }
 
+float Graphics2D::getCurMaxY()
+{
+	return curMax[stack].y;
+}
+
+void Graphics2D::setSubsection(subsection * s)
+{
+	if (stack + 1 == maxStack)
+	{
+		//TODO throw exception
+	}
+	else
+	{
+		stack++;
+		curMax[stack] = s->maxi;
+		glPushMatrix();
+		glMultMatrixf(s->mat);
+	}
+}
+
+void Graphics2D::resetLastSubsection()
+{
+	glPopMatrix();
+	stack--;
+}
+void Graphics2D::resetAllSubsections()
+{
+	while (stack > 0)
+	{
+		glPopMatrix();
+		stack--;
+	}
+	stack = 0;
+}
+
+subsection Graphics2D::generateSubsection(float snapx, float snapy, float sizex, float sizey, int snappoint)
+{
+	subsection ret;
+	float size = sizex<sizey?sizex:sizey;
+	float newSizex = sizex / size;
+	float newSizey = sizey / size;
+	ret.maxi.x = newSizex;
+	ret.maxi.y = newSizey;
+	float oldSizex = curMax[stack].x;
+	float oldSizey = curMax[stack].y;
+	int locx = (snappoint % 3) - 1;
+	int locy = (snappoint / 3) - 1;
+	float oldSelectedx = oldSizex*locx;
+	float oldSelectedy = oldSizey*locy;
+	float newSelectedx = newSizex*locx;
+	float newSelectedy = newSizey*locy;
+	glPushMatrix();//save the matrix to create a new matrix from scratch
+	glLoadMatrixf(emptyMat);
+	glTranslatef(oldSelectedx + snapx - newSelectedx*size*0.5f, oldSelectedy + snapy - newSelectedy*size*0.5f, 0.0f);
+	glScalef(size*0.5f, size*0.5f, 1.0f);
+	glGetFloatv(GL_MODELVIEW_MATRIX, ret.mat);
+	glPopMatrix();//restore matrix
+	return ret;
+}
+
+subsection Graphics2D::generateSubsection(float snapx, float snapy, float sizex, float sizey, int snappoint,float angle)
+{
+	subsection ret;
+	float size = sizex<sizey ? sizex : sizey;
+	float newSizex = sizex / size;
+	float newSizey = sizey / size;
+	float oldSizex = curMax[stack].x;
+	float oldSizey = curMax[stack].y;
+	int locx = (snappoint % 3) - 1;
+	int locy = (snappoint / 3) - 1;
+	float oldSelectedx = oldSizex*locx;
+	float oldSelectedy = oldSizey*locy;
+	float newSelectedx = newSizex*locx;
+	float newSelectedy = newSizey*locy;
+	glPushMatrix();//save the matrix to create a new matrix from scratch
+	glLoadMatrixf(emptyMat);
+	glTranslatef(oldSelectedx + snapx - newSelectedx*size*0.5f, oldSelectedy + snapy - newSelectedy*size*0.5f, 0.0f);
+	glScalef(size*0.5f, size*0.5f, 1.0f);
+	glRotatef(angle, 0, 0, 1.0f);
+	glGetFloatv(GL_MODELVIEW_MATRIX, ret.mat);
+	glPopMatrix();//restore matrix
+	return ret;
+}
 void Graphics2D::setColor(float r, float g, float b)
 {
 	glColor3f(r, g, b);
@@ -139,5 +237,5 @@ void Graphics2D::fillOval(float xCenter, float yCenter, float xRad, float yRad)
 
 void Graphics2D::drawString(char * str, float x, float y)
 {
-	//glutStrokeCharacter(GLUT_STROKE_ROMAN, str[0]);
+	//glutStrokeCharacter(GLUT_STROKE_ROMAN, str[0]);//TODO find alternative
 }
