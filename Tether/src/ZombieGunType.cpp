55a61ad3375550b2e2d2f3ccbbac195b51d3f250
HL65536 <HL65536@web.de>
2017-12-12 00:32:37 +0100

    test commit - added gun type

---
+++ Tether/src/ZombieGunType.cpp
@@ -0,0 +1,21 @@
+/*
+ * ZombieGunType.cpp
+ *
+ *  Created on:	12.12.2017
+ *      Author:	HL65536
+ *     Version:	2.0
+ */
+
+#include "ZombieGunType.h"
+
+Zombie_GunType::Zombie_GunType()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Zombie_GunType::~Zombie_GunType()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Tether/src/ZombieGunType.h
@@ -0,0 +1,19 @@
+/*
+ * ZombieGunType.h
+ *
+ *  Created on:	12.12.2017
+ *      Author:	HL65536
+ *     Version:	2.0
+ */
+
+#ifndef SRC_ZOMBIEGUNTYPE_H_
+#define SRC_ZOMBIEGUNTYPE_H_
+
+class Zombie_GunType
+{
+public:
+	Zombie_GunType();
+	~Zombie_GunType();
+};
+
+#endif /* SRC_ZOMBIEGUNTYPE_H_ */
