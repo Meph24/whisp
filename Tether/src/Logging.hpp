8606eee03ad8945cc2d17b8fae4e5e99bf445fc4
Meph24 <robert.graf24@gmail.com>
2019-11-16 04:55:19 +0100

    Custom overload for high_resolution_clock logging.

--- Tether/src/Logging.hpp
+++ Tether/src/Logging.hpp
@@ -20,10 +20,8 @@ using std::ostream;
 using std::string;
 using std::unique_ptr;
 
-
 namespace log
 {
-using SysTp = std::chrono::time_point<std::chrono::system_clock>;
 
 enum TYPE
 {
@@ -99,22 +97,39 @@ template<typename clock_type>
 class TimeOutputWrapper
 {
 	using outTp = std::chrono::time_point<clock_type>;
-	const outTp& tp;
-	string format;
 public:
-	TimeOutputWrapper(const outTp& tp, string format = "%F %T")
-		: tp(tp)
-		, format(format)
+	string format;
+	const outTp& tp;
+	//sadly format string cannot be specified by default parameter
+	// because default parameters do not work with template specialization
+	TimeOutputWrapper(const outTp& tp)
+		: format("%F %T")
+		, tp(tp)
 	{}
 
-	friend ostream& operator<< (ostream& os, const TimeOutputWrapper& tow)
-	{
-		std::time_t now_c = clock_type::to_time_t(tow.tp);
-		os << std::put_time(std::localtime(&now_c), tow.format.c_str());
-		return os;
-	}
-
+	TimeOutputWrapper(const outTp& tp, string format)
+		: format(format)
+		, tp(tp)
+	{}
 };
+
+template<typename clock_type>
+ostream& operator<< (ostream& os, const TimeOutputWrapper<clock_type>& tow)
+{
+	std::time_t now_c = clock_type::to_time_t(tow.tp);
+	os << std::put_time(std::localtime(&now_c), tow.format.c_str());
+	return os;
+}
+
+template<>
+ostream& operator<< (ostream& os, const TimeOutputWrapper<std::chrono::high_resolution_clock>& tow)
+{
+	std::time_t now_c = std::chrono::high_resolution_clock::to_time_t(tow.tp);
+	os	<< std::put_time(std::localtime(&now_c), tow.format.c_str()) 
+		<< '.' << tow.tp.time_since_epoch().count() % 1000000000;
+	return os;
+}
+
 
 } /* namespace log */
 
