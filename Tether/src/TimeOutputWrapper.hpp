2a2647432b7d6cc84b2c44432378f439d7bdbd1f
Meph24 <robert.graf24@gmail.com>
2019-11-20 23:31:18 +0100

    Hopefully fixed an ERROR for that SOMEONE AGAIN!
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Tether/src/TimeOutputWrapper.hpp
+++ Tether/src/TimeOutputWrapper.hpp
@@ -38,6 +38,14 @@ ostream& operator<< (ostream& os, const TimeOutputWrapper<clock_type>& tow)
 	return os;
 }
 
+template<>
+ostream& operator<< (ostream& os, const TimeOutputWrapper<std::chrono::high_resolution_clock>& tow)
+{
+	std::time_t now_c = std::chrono::high_resolution_clock::to_time_t(tow.tp);
+	os	<< std::put_time(std::localtime(&now_c), tow.format.c_str()) 
+		<< '.' << tow.tp.time_since_epoch().count() % 1000000000;
+	return os;
+}
 
 #endif /* TIMEOUTPUTWRAPPER_HPP */
 
