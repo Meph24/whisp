d9fca55f94633871f4e58e2283260256f6ba26a8
Meph24 <robert.graf24@gmail.com>
2020-08-19 09:08:52 +0200

    Readded the unit tests for the time module.

---
+++ Tether/test/test_FloatSeconds.cpp
@@ -0,0 +1,29 @@
+#include "../src/FloatSeconds.hpp"
+
+#include <gtest/gtest.h>
+
+#include <chrono>
+
+#include <iostream>
+
+using namespace std::chrono;
+
+TEST(test_FloatSeconds, Float_Accuracy)
+{
+	nanoseconds ns(1543212345);
+	double d = (double)ns.count();
+	d /= 1000000000;
+	float manually_converted_float_seconds = (float) d;
+	FloatSeconds fs = ns;
+	ASSERT_EQ(ns, fs);
+	//LE = not greater than
+	//conversion should not produce an error
+	float diff = fs.count() - manually_converted_float_seconds;
+	ASSERT_EQ(diff, 0.0f);
+}
+
+int main (int argc , char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
