b9b3ce00eb0bb35ab19c977c51ebd8567798fcd2
Meph24 <robert.graf24@gmail.com>
2021-04-08 19:42:52 +0200

    small safeguard for last commit

--- Tether/src/Simulation.cpp
+++ Tether/src/Simulation.cpp
@@ -59,7 +59,8 @@ EntityPlayer* Simulation::userAvatar(SimulationUser* user)
 void Simulation::disconnectAvatarFrom( SimulationUser* to_kick_user )
 { 
 	lock_guard lg_ (players_lock);
-	players[to_kick_user]->setUser(nullptr);
+	if(players.find(to_kick_user) != players.end() )
+		players[to_kick_user]->setUser(nullptr);
 	players.erase(to_kick_user);
 }
 
